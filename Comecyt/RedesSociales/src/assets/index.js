959d00fc8919e3aefb3c73e6db428fd91f232fbe
Ismael Antonio Gonzalez <107378053+keiv34@users.noreply.github.com>
2026-06-29 22:31:12 -0600

    Create index.js

---
+++ Comecyt/RedesSociales/src/assets/index.js
@@ -0,0 +1,11 @@
+import placeholder from "./placeholder.png";
+
+export const fbIntroImg = placeholder;
+export const requisitosImg = placeholder;
+export const whatsappImg = placeholder;
+export const instagramImg = placeholder;
+export const marketplaceImg = placeholder;
+export const seguridadImg = placeholder;
+export const perfilImg = placeholder;
+export const correoImg = placeholder;
+export const gmailImg = placeholder;
